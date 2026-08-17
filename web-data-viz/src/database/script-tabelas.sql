b186b42f9d1dfce2104228a4023550def4fdffe9
sarahtiemi2508 <sarah.sato@sptech.school>
2026-08-17 15:31:44 -0300

    Inicia script de banco de dados

--- web-data-viz/src/database/script-tabelas.sql
+++ web-data-viz/src/database/script-tabelas.sql
@@ -1,32 +1,90 @@
-CREATE DATABASE empresa(
+CREATE TABLE empresa(
 	id PRIMARY KEY AUTO_INCREMENT, 
 	razaoSocial VARCHAR(100),
-	cnp CHAR(14)
+	nomeFantasia VARCHAR(100),
+	cnpj CHAR(14),
+	segmentoAtuacao (80),
+	website VARCHAR(50),
+	email VARCHAR(50),
+	fkEndereco INT,
+	CONSTRAINT ctFkEndereco
+	FOREIGN KEY(fkEndereco)
+	REFERENCES endereco(id)
 );
 
-CREATE DATABASE funcionario(
+CREATE TABLE endereco(
+	endereco INT PRIMARY KEY AUTO_INCREMENT,
+	cep CHAR(8),
+	logradouro VARCHAR(30),
+	nome VARCHAR(70),
+	numero VARCHAR(20),
+	estado CHAR(2),
+	cidade VARCHAR(50)
+);
+
+CREATE TABLE funcionario(
 	id PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(60),
 	dtNascimento DATE,
+	emailCorporativo VARCHAR(50),
+	telefone VARCHAR()
 	cpf CHAR(11),
 	cargo VARCHAR(30),
+	departamento VARCHAR(40),
+	senha VARCHAR(40),
+	
 	fkEmpresa INT,
 	CONSTRAINT ctFkEmpresa
 	FOREIGN KEY (fkEmpresa)
-	REFERENCES empresa(id)
-);
-
-CREATE DATABASE permissoes(
-	permissao VARCHAR(40)
-);
-
-CREATE DATABASE funcionario_permissao(
-	fkFuncionario INT,
+	REFERENCES empresa(id),
 	fkPermissao INT,
-	CONSTRAINT ctFkFuncionario
-	FOREIGN KEY (fkFuncionario)
-	REFERENCES funcionario(id),
 	CONSTRAINT ctFkPermissao
 	FOREIGN KEY (fkPermissao)
-	REFERENCES permissoes(id)
+	REFERENCES permissao(id)
 );
+
+CREATE TABLE permissao(
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+	permissao VARCHAR(40),
+	especificacao VARCHAR(100)
+);
+
+-- CREATE TABLE aviao(
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+-- 	prefixo VARCHAR(50),
+-- 	companhia VARCHAR(60),
+-- );
+
+-- CREATE TABLE componentes(
+-- 	id INT PRIMARY KEY AUTO_INCREMENT,
+
+-- 	fkAviao INT,
+-- 	CONSTRAINT ctFkAviao
+-- 	FOREIGN KEY (fkAviao)
+-- 	REFERENCES aviao(id),
+-- 	fkComputador INT,
+-- 	CONSTRAINT ctFkComputador 
+-- 	FOREIGN KEY (fkComputador)
+-- 	REFERENCES computador(id),
+
+-- );
+
+-- CREATE TABLE computador(
+-- 	id PRIMARY KEY AUTO_INCREMENT,
+
+
+-- );
+
+-- CREATE TABLE parametros(
+-- 	id PRIMARY KEY AUTO_INCREMENT,
+
+-- 	fkComponente INT<
+-- 	CONSTRAINT ctFkComponente
+-- 	FOREIGN KEY (fkComponente)
+-- 	REFERENCES 
+-- );
+
+INSERT INTO permissao (permissao, especificacao) VALUES
+("Acesso total", "Cadastros, permissões, relatórios, configurações"),
+("Acesso operacional", "Sem gerenciamento de usuários"),
+("Visualização e acompanhamento", "Visualização da dashboard e acompanhamento de alertas"),
